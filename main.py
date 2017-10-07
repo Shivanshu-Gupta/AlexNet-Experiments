13989134653b6a7637f58a9eefe3687e164c1729
Shivanshu Gupta <shivanshugupta1995@gmail.com>
2017-10-08 01:48:42 +0530

    Added checkpointing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import pprint
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -10,19 +11,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch.utils.model_zoo as model_zoo
 from torch.utils.data import DataLoader
-from dataset import AlexnetDataset, imshow
+from dataset import AlexnetDataset, imshow, remove_exif
 from network import alexnet
 from train import train_model
+
+pp = pprint.PrettyPrinter()
 
 plt.ion()
 
 parser = argparse.ArgumentParser(description='Alexnet Experiment')
-parser.add_argument('--data_root', dest='data_root', default='/home/cse/dual/cs5130298/scratch/ImageNet_Subset/')
-parser.add_argument('--remove_exif', dest='remove_exif', action='store_true', default=False)
-parser.add_argument('--usegpu', dest='use_gpu', action='store_true', default=False)
-parser.add_argument('--epochs', dest='num_epochs', type=int, default=20)
-parser.add_argument('--lr', dest='lr', type=float, default=0.01)
-parser.add_argument('--step', dest='step_size', type=int, default=10)
+parser.add_argument('--data_root', default='/home/cse/dual/cs5130298/scratch/ImageNet_Subset/')
+parser.add_argument('--remove_exif', action='store_true', default=False)
+parser.add_argument('--use_gpu', action='store_true', default=False)
+parser.add_argument('--epochs', default=40, type=int, metavar='N')
+parser.add_argument('--lr', default=0.01, type=float, metavar='LR')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M')
+parser.add_argument('--step_size', default=10, type=int, metavar='N')
+parser.add_argument('--save_dir', default='checkpoints/', type=str, metavar='PATH')
+parser.add_argument('--reload', default='', type=str, metavar='PATH',
+                    help='path to checkpoint to load (default: none)')
+# parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+#                     help='manual epoch number (useful on restarts)')
+parser.add_argument('--test', dest='test', action='store_true',
+                    help='test model on test set')
 args = parser.parse_args()
 
 use_gpu = args.use_gpu
@@ -59,22 +70,46 @@ class_names = image_datasets['train'].classes
 print(class_names)
 print(dataset_sizes)
 
-if __name__=='__main__':
+
+def save_params():
+    os.makedirs(args.save_dir, exist_ok=True)
+    param_file = args.save_dir + '/' + 'params.pt'
+    torch.save(args, param_file)
+
+
+if __name__ == '__main__':
+    pp.pprint(args)
+    save_params()
+
     if args.remove_exif:
         for phase in phases:
             remove_exif(root, phase)
-    model = alexnet(pretrained=False, num_classes=len(class_names))
-
     if use_gpu:
         model = model.cuda()
 
     criterion = nn.CrossEntropyLoss()
 
+    if args.reload:
+        if os.path.isfile(args.reload):
+            print("=> loading checkpoint '{}'".format(args.reload))
+            checkpoint = torch.load(args.reload)
+            args.start_epoch = checkpoint['epoch']
+            best_acc = checkpoint['best_acc']
+            model.reload_state_dict(checkpoint['state_dict'])
+            # optimizer.reload_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.reload, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.reload))
+    else:
+        model = alexnet(pretrained=False, num_classes=len(class_names))
+
     # Observe that all parameters are being optimized
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     # Decay LR by a factor of gamma every step_size epochs
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=1)
+
     # TODO: Store the parameters and use them to initialise next time.
-    model = train_model(model, dataloaders, dataset_sizes, criterion, optimizer, exp_lr_scheduler,
-                           num_epochs=args.num_epochs, use_gpu=use_gpu)
+    model = train_model(model, dataloaders, criterion, optimizer, exp_lr_scheduler, args.save_dir,
+        num_epochs=args.epochs, use_gpu=use_gpu)
--- train.py
+++ train.py
@@ -1,8 +1,77 @@
+import shutil
 import time
 import torch
 from torch.autograd import Variable
 
-def train_model(model, dataloaders, dataset_sizes, criterion, optimizer, scheduler, num_epochs=25, use_gpu=False):
+def train(model, train_loader, criterion, optimizer, use_gpu=False):
+    model.train()  # Set model to training mode
+
+    running_loss = 0.0
+    running_corrects = 0
+
+    # Iterate over data.
+    for (inputs, labels) in train_loader:
+        # wrap input, labels in Variable
+        if use_gpu:
+            inputs = Variable(inputs.cuda())
+            labels = Variable(labels.cuda())
+        else:
+            inputs, labels = Variable(inputs), Variable(labels)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward
+        outputs = model(inputs)
+        _, preds = torch.max(outputs.data, 1)
+        loss = criterion(outputs, labels)
+
+        # backward + optimize
+        loss.backward()
+        optimizer.step()
+
+        # statistics
+        running_loss += loss.data[0]
+        running_corrects += torch.sum(preds == labels.data)
+
+    epoch_loss = running_loss / len(train_loader)
+    epoch_acc = running_corrects / len(train_loader)
+
+    print('Train Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
+    return epoch_loss, epoch_acc
+
+def validate(model, val_loader, criterion, use_gpu=False):
+    model.eval()  # Set model to evaluate mode
+    running_loss = 0.0
+    running_corrects = 0
+
+    # Iterate over data.
+    for (inputs, labels) in val_loader:
+        # wrap input, labels in Variable
+        if use_gpu:
+            inputs = Variable(inputs.cuda())
+            labels = Variable(labels.cuda())
+        else:
+            inputs, labels = Variable(inputs), Variable(labels)
+
+        # forward
+        outputs = model(inputs)
+        _, preds = torch.max(outputs.data, 1)
+        loss = criterion(outputs, labels)
+
+        # statistics
+        running_loss += loss.data[0]
+        running_corrects += torch.sum(preds == labels.data)
+
+    epoch_loss = running_loss / len(val_loader)
+    epoch_acc = running_corrects / len(val_loader)
+
+    print('Validation Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
+
+    return epoch_loss, epoch_acc
+
+
+def train_model(model, data_loaders, criterion, optimizer, scheduler, save_dir, num_epochs=25, use_gpu=False):
     print('Training Model with use_gpu={}...'.format(use_gpu))
     since = time.time()
 
@@ -13,58 +82,22 @@ def train_model(model, dataloaders, dataset_sizes, criterion, optimizer, schedul
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
 
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'validation']:
-            if phase == 'train':
-                scheduler.step()
-                model.train(True)  # Set model to training mode
-            else:
-                model.train(False)  # Set model to evaluate mode
+        scheduler.step()
+        train(model, data_loaders['train'], criterion, optimizer, use_gpu)
+        epoch_loss, epoch_acc = validate(model, data_loaders['validation'], criterion, use_gpu)
+        # TODO: Use TensorBoard to record metrics.
+        # deep copy the model
+        is_best = epoch_acc > best_acc
+        if is_best:
+            best_acc = epoch_acc
+            best_model_wts = model.state_dict()
 
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for data in dataloaders[phase]:
-                # get the inputs
-                inputs, labels = data
-                # print(labels)
-                # wrap them in Variable
-                if use_gpu:
-                    inputs = Variable(inputs.cuda())
-                    labels = Variable(labels.cuda())
-                else:
-                    inputs, labels = Variable(inputs), Variable(labels)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                outputs = model(inputs)
-                _, preds = torch.max(outputs.data, 1)
-                loss = criterion(outputs, labels)
-
-                # backward + optimize only if in training phase
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
-
-                # statistics
-                running_loss += loss.data[0]
-                running_corrects += torch.sum(preds == labels.data)
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects / dataset_sizes[phase]
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
-            # TODO: Use TensorBoard to record metrics.
-            # deep copy the model
-            if phase == 'validation' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = model.state_dict()
-            
-        print()
+        save_checkpoint(save_dir, {
+            'epoch': epoch + 1,
+            'best_acc': best_acc,
+            'state_dict': model.state_dict(),
+            # 'optimizer': optimizer.state_dict(),
+        }, is_best)
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
@@ -74,3 +107,10 @@ def train_model(model, dataloaders, dataset_sizes, criterion, optimizer, schedul
     # load best model weights
     model.load_state_dict(best_model_wts)
     return model
+
+
+def save_checkpoint(save_dir, state, is_best):
+    savepath = save_dir + '/' + 'checkpoint.pth.tar'
+    torch.save(state, savepath)
+    if is_best:
+        shutil.copyfile(savepath, save_dir + '/' + 'model_best.pth.tar')
